b420190dc0d7c0039aa6ad7a045bd25c53b47447
Branoli <lar.pasha.chenko@gamil.com>
2018-03-24 11:03:35 +0500

    add url db/search/

--- testly_db_app/urls.py
+++ testly_db_app/urls.py
@@ -5,5 +5,6 @@ from testly_db_app import views
 urlpatterns = [
     url(r'^db/get$', views.get_all),
     url(r'^db/post$', views.post_element),
-    url(r'^db/(?P<pk>[0-9]+)/')
+    url(r'^db/get_subtree/(?P<pk>[0-9]+)/', views.get_subtree),
+    url(r'^db/search/(?P<text>[a-zA-Zа-яА-я]+)/$', views.get_search_text),
 ]
