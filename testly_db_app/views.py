8f16db9fa2a975ae4fe4c6086de69377956444d2
Branoli <lar.pasha.chenko@gamil.com>
2018-03-24 10:22:02 +0500

    add method post_element

--- testly_db_app/views.py
+++ testly_db_app/views.py
@@ -2,6 +2,7 @@ from .models import WoodTable
 from .serializers import WoodTableSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 
 @api_view(['GET'])
@@ -11,3 +12,14 @@ def get_all(request):
     serializer = WoodTableSerializer(snippets,
                                      many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def post_element(request):
+
+    serializer = WoodTableSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
