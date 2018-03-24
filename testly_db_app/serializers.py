cc2023b859a16d303a3023d3a33194a4faf9fe78
Branoli <lar.pasha.chenko@gamil.com>
2018-03-24 10:19:05 +0500

    renamed AllWoodTableSerializer on WoodTableSerializer

--- testly_db_app/serializers.py
+++ testly_db_app/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import WoodTable
 
 
-class AllWoodTableSerializer(serializers.ModelSerializer):
+class WoodTableSerializer(serializers.ModelSerializer):
     class Meta:
         model = WoodTable
         fields = ['id', 'text', 'parent', 'tree_id', 'level']
